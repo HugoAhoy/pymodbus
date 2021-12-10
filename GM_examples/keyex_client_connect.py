153587b219f38a9215077f6a7f398d0b2de5af38
HugoAhoy <zhjd3302@outlook.com>
2021-12-10 16:13:52 +0800

    add 2 example only contain connect and handle. They're used to check the key exchange process.

---
+++ GM_examples/keyex_client_connect.py
@@ -0,0 +1,31 @@
+from pymodbus.client.sync import ModbusTcpClient as ModbusClient
+from pymodbus.key_exchange_message import EclipsePointResponse, EclipsePointRequest
+
+# --------------------------------------------------------------------------- #
+# configure the client logging
+# --------------------------------------------------------------------------- #
+import logging
+FORMAT = ('%(asctime)-15s %(threadName)-15s '
+          '%(levelname)-8s %(module)-15s:%(lineno)-8s %(message)s')
+logging.basicConfig(format=FORMAT)
+log = logging.getLogger()
+log.setLevel(logging.DEBUG)
+
+UNIT = 0x1
+
+def run_sync_client():
+    client = ModbusClient('localhost', port=5020)
+
+    # Register key_message to ClientDecoder
+    client.register(EclipsePointResponse)
+
+    client.connect()
+
+    # ----------------------------------------------------------------------- #
+    # close the client
+    # ----------------------------------------------------------------------- #
+    client.close()
+
+
+if __name__ == "__main__":
+    run_sync_client()
---
+++ GM_examples/keyex_server_handle.py
@@ -0,0 +1,59 @@
+# --------------------------------------------------------------------------- #
+# import the various server implementations
+# --------------------------------------------------------------------------- #
+from pymodbus.version import version
+from pymodbus.server.sync import StartTcpServer
+
+from pymodbus.device import ModbusDeviceIdentification
+from pymodbus.datastore import ModbusSequentialDataBlock, ModbusSparseDataBlock
+from pymodbus.datastore import ModbusSlaveContext, ModbusServerContext
+
+from pymodbus.transaction import ModbusRtuFramer, ModbusBinaryFramer
+from pymodbus.key_exchange_message import EclipsePointRequest
+
+# --------------------------------------------------------------------------- #
+# configure the service logging
+# --------------------------------------------------------------------------- #
+import logging
+FORMAT = ('%(asctime)-15s %(threadName)-15s'
+          ' %(levelname)-8s %(module)-15s:%(lineno)-8s %(message)s')
+logging.basicConfig(format=FORMAT)
+log = logging.getLogger()
+log.setLevel(logging.DEBUG)
+
+
+def run_server():
+    # ----------------------------------------------------------------------- #
+    # initialize your data store
+    # ----------------------------------------------------------------------- #
+    store = ModbusSlaveContext(
+        di=ModbusSequentialDataBlock(0, [17]*100),
+        co=ModbusSequentialDataBlock(0, [17]*100),
+        hr=ModbusSequentialDataBlock(0, [17]*100),
+        ir=ModbusSequentialDataBlock(0, [17]*100))
+
+    context = ModbusServerContext(slaves=store, single=True)
+
+    # ----------------------------------------------------------------------- #
+    # initialize the server information
+    # ----------------------------------------------------------------------- #
+    # If you don't set this or any fields, they are defaulted to empty strings.
+    # ----------------------------------------------------------------------- #
+    identity = ModbusDeviceIdentification()
+    identity.VendorName = 'Pymodbus'
+    identity.ProductCode = 'PM'
+    identity.VendorUrl = 'http://github.com/riptideio/pymodbus/'
+    identity.ProductName = 'Pymodbus Server'
+    identity.ModelName = 'Pymodbus Server'
+    identity.MajorMinorRevision = version.short()
+
+    # ----------------------------------------------------------------------- #
+    # run the server you want
+    # ----------------------------------------------------------------------- #
+    # Tcp:
+    # use custom_functions to register EclipsePointRequest on ServerDecoder
+    StartTcpServer(context, identity=identity, address=("", 5020),custom_functions=[EclipsePointRequest])
+
+if __name__ == "__main__":
+    run_server()
+
